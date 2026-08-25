511da24d89f19e43bd49b2a07b738b0a8a7b13d5
oqaja <67749668+oqaja@users.noreply.github.com>
2026-08-25 12:01:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker untuk Absensi Karyawan PWA
 // Naikkan versi ini (v1 -> v2 -> ...) tiap kali update index.html biar cache lama dibuang otomatis
-const CACHE_NAME = 'absensi-cache-v37';
+const CACHE_NAME = 'absensi-cache-v38';
 
 const APP_SHELL = [
   './',
